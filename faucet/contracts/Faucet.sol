5fb23875118298d53c93637ccf35099cdb56c25b
Hassan Saber <hassansaberkarin@gmail.com>
2024-09-02 09:31:24 +0330

    004 First Smart Contract

---
+++ faucet/contracts/Faucet.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.19 <0.9.0;
+
+contract Faucet {
+
+  // Storage variables
+  uint public funds = 1000; //unit = int256 
+  int public counter = -10; 
+
+}
