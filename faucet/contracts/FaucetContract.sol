85c4eb06b57031299de378d48b396940bbb00e7a
Hassan Saber <hassansaberkarin@gmail.com>
2024-09-09 19:27:07 +0330

    S7 done

--- faucet/contracts/FaucetContract.sol
+++ faucet/contracts/FaucetContract.sol
@@ -4,9 +4,23 @@ pragma solidity >=0.8.19 <0.9.0;
 contract Faucet {
 
   // Storage variables
-  uint public funds = 1000; //unit = uint256 =10100...
-  int public counter = -10; 
-  uint32 public test = 4294967295;
+  // uint public funds = 1000; //unit = uint256 =10100...
+  // int public counter = -10; 
+  // uint32 public test = 4294967295;
 
+
+  receive() external payable {} 
+
+  function addFunds() external payable {}
+
+  function justTesting() external pure returns(uint){
+    return 2 + 2;
+  }
 
 }
+
+
+
+// const instance = await Faucet.deployed()
+// truffle migrate --reset
+//  truffle console
