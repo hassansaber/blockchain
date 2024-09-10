5996da1803a5ebad2cc1c33b9d4ae06c6c059597
Hassan Saber <hassansaberkarin@gmail.com>
2024-09-10 16:54:29 +0330

    .

--- faucet/contracts/FaucetContract.sol
+++ faucet/contracts/FaucetContract.sol
@@ -2,24 +2,31 @@
 pragma solidity >=0.8.19 <0.9.0;
 
 contract Faucet {
+    // Storage variables
+    // uint public funds = 1000; //unit = uint256 =10100...
+    // int public counter = -10;
+    // uint32 public test = 4294967295;
 
-  // Storage variables
-  // uint public funds = 1000; //unit = uint256 =10100...
-  // int public counter = -10; 
-  // uint32 public test = 4294967295;
+    address[] public funders;
 
+    receive() external payable {}
 
-  receive() external payable {} 
+    function addFunds() external payable {
+        funders.push(msg.sender);
+    }
 
-  function addFunds() external payable {}
+    function justTesting() external pure returns (uint) {
+        return 2 + 2;
+    }
 
-  function justTesting() external pure returns(uint){
-    return 2 + 2;
-  }
+    function getAllFunders() external view returns (address[] memory) {
+        return funders;
+    }
 
+    function getFunderAtIndex(uint8 index) external view returns (address) {
+        address[] memory _funders = getAllFunders();
+    }
 }
-
-
 
 // const instance = await Faucet.deployed()
 // truffle migrate --reset
