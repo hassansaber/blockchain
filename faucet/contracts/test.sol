660c09c6ec1740e58f3eae73c7a18fa922931d7f
hassansaber <hassansaberkarin@gmail.com>
2024-09-15 16:02:19 +0330

    ..

--- faucet/contracts/test.sol
+++ faucet/contracts/test.sol
@@ -21,7 +21,7 @@ contract Test {
 
 
 
-   function test3() external pure returns(bytes32 data){
+   function test2() external pure returns(bytes32 data){
 
     assembly{
       let fmp := mload(0x40)
